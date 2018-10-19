21953b17c406920fafe64447f9721e9684278fdc
Iván Hernández Ortega <ivan@wupplier.com>
2018-10-19 18:24:19 +0100

    :hammer: Refactor. Single file components

---
+++ src/components/MovieFilter.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="movie-filter">
+        <h2>Filter results</h2>
+        <div class="filter-group">
+            <check-filter :key="genre" @check-filter="checkFilter" :title="genre" v-for="genre in genres"></check-filter>
+        </div>
+    </div>
+</template>
+<script>
+import genres from "../util/genres";
+import CheckFilter from './CheckFilter.vue'
+
+export default {
+  name: "MovieFilter",
+  data() {
+    return {
+      genres
+    };
+  },
+  methods: {
+    checkFilter(category, title, checked) {
+      this.$emit("check-filter", category, title, checked);
+    }
+  },
+  components: {
+    CheckFilter
+  }
+};
+</script>
---
+++ src/components/MovieList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="movie-list">
+        <div :key="movie.title" v-for="movie in filteredMovies" class="movie">{{movie.title}}</div>
+    </div>
+</template>
+<script>
+import genres from '../util/genres'
+
+export default {
+  name: "MovieList",
+  props: ["genre", "time"],
+  data() {
+    return {
+      movies: [
+        { title: "Pulp fiction", genre: genres.CRIME },
+        { title: "Home Alone", genre: genres.COMEDY },
+        { title: "Austin Powers", genre: genres.COMEDY }
+      ]
+    };
+  },
+  methods: {
+    moviePassesGenreFilter(movie) {
+      if (!this.genre.length) return true;
+      return this.genre.findIndex(genre => genre === movie.genre) > -1;
+    }
+  },
+  computed: {
+    filteredMovies() {
+      return this.movies.filter(this.moviePassesGenreFilter);
+    }
+  }
+};
+</script>
+<style lang="scss">
+</style>
